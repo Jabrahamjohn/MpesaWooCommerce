c08512f6dbefab910b06384923477f6d945ea229
Abraham John <jabrahamjohns@gmail.com>
2024-07-18 10:18:38 +0300

    boiler plate

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'c5d322a33ebfcb57562fd593cc5e04e926900987',
+        'name' => 'osenco/wc-mpesa',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'osenco/wc-mpesa' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'c5d322a33ebfcb57562fd593cc5e04e926900987',
+            'dev_requirement' => false,
+        ),
+    ),
+);
